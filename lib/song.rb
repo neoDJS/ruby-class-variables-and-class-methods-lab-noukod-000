58d19e24e1a3c6fad19fe726a3bb5ca5057ccb07
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-03 20:29:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,11 +16,11 @@ class Song
   end
 
   def self.artists
-    @@artists.unique
+    @@artists.uniq
   end
 
   def self.genres
-    @@genres.unique
+    @@genres.uniq
   end
 
   def self.genre_count
